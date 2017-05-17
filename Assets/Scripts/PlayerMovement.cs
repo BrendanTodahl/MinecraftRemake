9c37906d2f16b3b0fd365dc411fd74c938069341
Brendan Todahl <Btodahl29@gmail.com>
2017-05-17 17:13:22 -0400

    Added some initial xbox controller support.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour {
 
     private const float speed = 5.0f;
     private const float jumpForce = 250.0f;
+    private float lookSpeed = 5f;
 
     private float elevateAngle;
     private new GameObject camera;
@@ -39,14 +40,14 @@ public class PlayerMovement : MonoBehaviour {
         Camera = transform.FindChild("Main Camera").gameObject;
         Cursor.lockState = CursorLockMode.Locked;
         ElevateAngle = 0.0f;
-	}
+    }
     
     void FixedUpdate () {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(inputX, 0.0f, inputY) * speed * Time.deltaTime);
 
-        ElevateAngle += -Input.GetAxis("Mouse Y");
+        ElevateAngle += -Input.GetAxis("Mouse Y") * lookSpeed;
         if (ElevateAngle < -90.0f)
         {
             ElevateAngle = -90.0f;
@@ -57,7 +58,7 @@ public class PlayerMovement : MonoBehaviour {
         }
         Camera.transform.localEulerAngles = new Vector3(ElevateAngle, 0.0f, 0.0f);
 
-        float lookLeftRight = Input.GetAxis("Mouse X");
+        float lookLeftRight = Input.GetAxis("Mouse X") * lookSpeed;
         transform.Rotate(0.0f, lookLeftRight, 0.0f);
 
         if (Input.GetButtonDown("Jump") && isGrounded())
